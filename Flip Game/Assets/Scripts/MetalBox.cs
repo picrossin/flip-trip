2a2238d75614d5ed51f1411a9ce4d550dfb2cbe2
picrossin <jadengoter@gmail.com>
2020-02-15 16:10:53 -0600

    Added metal box

---
+++ Flip Game/Assets/Scripts/MetalBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalBox : MonoBehaviour
+{
+    public LayerMask collisionMask;
+    public bool nearWall = false;
+    public bool rotationLocked = true;
+    Flip flip;
+
+    void Start()
+    {
+        flip = GameObject.FindObjectOfType(typeof(Flip)) as Flip;
+    }
+
+    void Update()
+    {
+        if (flip.flipping)
+        {
+            GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            GetComponent<Rigidbody2D>().isKinematic = false;
+            GetComponent<Rigidbody2D>().freezeRotation = rotationLocked;
+        }
+    }
+}

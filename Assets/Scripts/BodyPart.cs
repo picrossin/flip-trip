5006b66a14af2cea8d4c407ba3939af16210cc46
picrossin <jadengoter@gmail.com>
2020-02-16 00:43:33 -0600

    Created explosion of body parts raining down upon the children in the land of Flip Trip

--- Assets/Scripts/BodyPart.cs
+++ Assets/Scripts/BodyPart.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class BodyPart : MonoBehaviour
 {
+    public int bodyPart = 0;
+    public List<Sprite> partSprites = new List<Sprite>();
+
+    SpriteRenderer renderer;
     float speed = 50f;
 
     void Start()
     {
-        speed = Random.Range(25f, 40f);
+        renderer = GetComponent<SpriteRenderer>();
+
+        speed = Random.Range(10f, 20f);
         GetComponent<Rigidbody2D>().velocity = Random.onUnitSphere * speed;
+
+        renderer.sprite = partSprites[bodyPart];
     }
 }
--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -18,7 +18,8 @@ public class Death : MonoBehaviour
     {
         for (int i = 0; i < 8; i++)
         {
-            Instantiate(bodyPart, transform.position + new Vector3(Random.Range(0, .3f), Random.Range(0, .3f), 0), Quaternion.Euler(Random.onUnitSphere));
+            GameObject part = Instantiate(bodyPart, transform.position + new Vector3(Random.Range(0, .3f), Random.Range(0, .3f), 0), Quaternion.Euler(Random.onUnitSphere));
+            part.GetComponent<BodyPart>().bodyPart = i;
         }
         yield return new WaitForSeconds(waitTime);
         loader = GameObject.FindObjectOfType(typeof(LevelLoader)) as LevelLoader;

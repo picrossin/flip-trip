43318f5f6483ce1acb9ec0cc3e0471a1904bdf16
picrossin <jadengoter@gmail.com>
2020-02-16 04:10:13 -0600

    Started making levels pretty with lights and shaders

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Door : MonoBehaviour
 {
     Flip flip;
     LevelLoader loader;
+    SpriteRenderer renderer;
+    Light2D light;
 
     void Start()
     {
         loader = GameObject.FindObjectOfType(typeof(LevelLoader)) as LevelLoader;
         flip = GameObject.FindObjectOfType(typeof(Flip)) as Flip;
+        renderer = GetComponent<SpriteRenderer>();
+        light = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        if (light != null)
+        {
+            if (flip.flipped)
+            {
+                renderer.color = Color.white;
+                light.enabled = true;
+            }
+            else
+            {
+                light.enabled = false;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
